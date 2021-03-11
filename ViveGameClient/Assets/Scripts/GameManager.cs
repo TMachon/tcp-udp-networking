562851e71b2a6420fa3e2ca31289445271de14a5
Theo Machon <machon@limsi.fr>
2021-03-11 16:32:12 +0100

    Vive project added

---
+++ ViveGameClient/Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public static Dictionary<int, PlayerManager> players = new Dictionary<int, PlayerManager>();
+    public static Dictionary<int, ItemSpawner> spawners = new Dictionary<int, ItemSpawner>();
+
+    public GameObject localPlayerPrefab;
+    public GameObject playerPrefab;
+    public GameObject itemSpawnerPrefab;
+
+    public static Dictionary<int, BuildingCube> buildingCubes = new Dictionary<int, BuildingCube>();
+    public GameObject buildingCubePrefab;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void SpawnPlayer(int _id, string _username, Vector3 _position, Quaternion _rotation)
+    {
+        GameObject _player;
+        if (_id == Client.instance.myId)
+        {
+            _player = Instantiate(localPlayerPrefab, _position, _rotation);
+        }
+        else
+        {
+            _player = Instantiate(playerPrefab, _position, _rotation);
+        }
+
+        _player.GetComponent<PlayerManager>().id = _id;
+        _player.GetComponent<PlayerManager>().username = _username;
+        players.Add(_id, _player.GetComponent<PlayerManager>());
+    }
+
+    public void CreateItemSpawner(int _spawnerId, Vector3 _position, bool _hasItem)
+    {
+        GameObject _spawner = Instantiate(itemSpawnerPrefab, _position, itemSpawnerPrefab.transform.rotation);
+        _spawner.GetComponent<ItemSpawner>().Initialize(_spawnerId, _hasItem);
+        spawners.Add(_spawnerId, _spawner.GetComponent<ItemSpawner>());
+    }
+
+    public void CreateBuildingCube(int _cubeId, Vector3 _position, Quaternion _rotation, Vector3 _scale)
+    {
+        GameObject _buildingCube = Instantiate(buildingCubePrefab, _position, _rotation);
+        _buildingCube.transform.localScale = _scale;
+
+        _buildingCube.GetComponent<BuildingCube>().Initialize(_cubeId, _position, _rotation, _scale);
+        buildingCubes.Add(_cubeId, _buildingCube.GetComponent<BuildingCube>());
+    }
+}
---
+++ ViveGameClient/Assets/Scripts/UnityNumericsConversion.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class UnityNumericsConversion : MonoBehaviour
+{
+    #region Unity to Numerics
+    public static UnityEngine.Vector3 getVector(System.Numerics.Vector3 _vector)
+	{
+        float _posX = _vector.X;
+        float _posY = _vector.Y;
+        float _posZ = _vector.Z;
+        return new UnityEngine.Vector3(_posX, _posY, _posZ);
+	}
+	
+	public static UnityEngine.Quaternion getQuaternion(System.Numerics.Quaternion _quaternion)
+	{
+        float _rotX = _quaternion.X;
+        float _rotY = _quaternion.Y;
+        float _rotZ = _quaternion.Z;
+        float _rotW = _quaternion.W;
+        return new UnityEngine.Quaternion(_rotX, _rotY, _rotZ, _rotW);
+	}
+    #endregion
+
+    #region Numerics to Unity
+    public static System.Numerics.Vector3 getVector(UnityEngine.Vector3 _vector)
+    {
+        float _posX = _vector.x;
+        float _posY = _vector.y;
+        float _posZ = _vector.z;
+        return new System.Numerics.Vector3(_posX, _posY, _posZ);
+    }
+
+    public static System.Numerics.Quaternion getQuaternion(UnityEngine.Quaternion _quaternion)
+    {
+        float _rotX = _quaternion.x;
+        float _rotY = _quaternion.y;
+        float _rotZ = _quaternion.z;
+        float _rotW = _quaternion.w;
+        return new System.Numerics.Quaternion(_rotX, _rotY, _rotZ, _rotW);
+    }
+    #endregion
+}
